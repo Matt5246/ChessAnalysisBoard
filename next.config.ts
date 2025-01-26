9bed0dfc7338b458bb65d66b2482413024ade62e
Matt5246 <116891485+Matt5246@users.noreply.github.com>
2025-01-26 23:54:38 +0100

    wip: stockfish

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    // Add Webpack loader for workers
+    config.module.rules.push({
+      test: /stockfish\.js$/, // Regex to match the worker file
+      use: { loader: 'worker-loader' },
+    });
+
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -20,7 +20,25 @@ const ChessboardComponent: FC<ChessboardComponentProps> = ({ fen }) => {
     useEffect(() => {
         setGame(new Chess(fen))
     }, [fen])
+    useEffect(() => {
+        const stockfish = new Worker("/stockfish.js");
+        const DEPTH = 8; // number of halfmoves the engine looks ahead
+        const FEN_POSITION =
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        const wasmPath = '/stockfish.wasm';
 
+        stockfish.postMessage({
+            type: 'init',
+            wasmPath,
+        });
+        stockfish.postMessage("uci");
+        stockfish.postMessage(`position fen ${FEN_POSITION}`);
+        stockfish.postMessage(`go depth ${DEPTH}`);
+
+        stockfish.onmessage = (e) => {
+            console.log(e.data); // in the console output you will see `bestmove e2e4` message
+        };
+    }, []);
     const safeGameMutate = (modify: (game: Chess) => void) => {
         setGame((g) => {
             const updatedGame = new Chess(g.fen()) // Ensure it's a Chess instance
